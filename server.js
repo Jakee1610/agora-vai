e516160c3bb611dbee38c136c8f1ddd5a96765f3
Jakee1610 <brian.dourado.david@gmail.com>
2025-06-09 20:57:03 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const host = '0.0.0.0';
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/fonts', express.static(path.join(__dirname, 'fonts')));
 
 const dataDir = path.join(__dirname, 'data');
 
